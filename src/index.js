9fd9499bbe41f91600e0a142331efad77a1a8958
Robert Koeze <rkoeze@gmail.com>
2017-05-09 22:50:56 -0700

    add mock router

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class MockRouter extends React.Component {
+
+  getChildContext() {
+    return (
+      {
+        router: {
+          route: {
+            location: this.props.location || "",
+            match: {
+              params: this.props.params || {},
+              path: this.props.path || ""
+            }
+          },
+          history: {
+            path: this.props.path || "",
+            createHref: function() {},
+            push: this.props.push || function() {},
+            replace: this.props.replace || function() {}
+          }
+        }
+      }
+    );
+  }
+
+  render() {
+    return (
+      // only allow MockRouter to render one child. Otherwise, throws error.
+      React.Children.only(this.props.children)
+    );
+  }
+}
+
+MockRouter.childContextTypes = {
+  router: PropTypes.object,
+}
